cc304de0f9e9925e2cf9c170a07df4161418f31a
voytechs <mark@slytechs.com>
2007-08-17 14:53:12 +0000

    Ported examples to work under linux env

--- trunk/examples/java/org/jnetpcap/examples/ClassicPcapExample.java
+++ trunk/examples/java/org/jnetpcap/examples/ClassicPcapExample.java
@@ -53,11 +53,11 @@ public class ClassicPcapExample {
 
 		int i = 0;
 		for (PcapIf device : alldevs) {
-			System.out.printf("#%d: %s [%s]\n", i++, device.getDescription(), device
-			    .getName());
+			System.out.printf("#%d: %s [%s]\n", i++, device.getName(), device
+			    .getDescription());
 		}
 
-		PcapIf device = alldevs.get(0); // We know we have atleast 1 device
+		PcapIf device = alldevs.get(2); // We know we have atleast 1 device
 		System.out.printf("\nChoosing '%s' on your behalf:\n", device
 		    .getDescription());
 	
