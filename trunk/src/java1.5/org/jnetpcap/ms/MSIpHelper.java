df47d18357d17750c0a072a8a90dd90689c75016
voytechs <voytechs@905ad56a-9210-0410-a722-a9c141187570>
2008-10-03 01:03:34 +0000

    Added JNumber class and JNI peers

--- trunk/src/java1.5/org/jnetpcap/ms/MSIpHelper.java
+++ trunk/src/java1.5/org/jnetpcap/ms/MSIpHelper.java
@@ -12,21 +12,46 @@
  */
 package org.jnetpcap.ms;
 
-import org.jnetpcap.PcapInteger;
+import org.jnetpcap.JNumber;
 
 /**
+ * The Internet Protocol Helper (IP Helper) API enables the retrieval and
+ * modification of network configuration settings for the local computer. Where
+ * Applicable The IP Helper API is applicable in any computing environment where
+ * programmatically manipulating TCP/IP configuration is useful. Typical
+ * applications include IP routing protocols and Simple Network Management
+ * Protocol (SNMP) agents. Developer Audience
+ * 
+ * @since 1.2
  * @author Mark Bednarczyk
  * @author Sly Technologies, Inc.
  */
-public class MSIpHelper {
+public final class MSIpHelper {
 
 	/**
+	 * Prevent instantiation
+	 */
+	private MSIpHelper() {
+		// Emtpy
+	}
+
+	/**
+	 * The GetInterfaceInfo function obtains the list of the network interface
+	 * adapters with IPv4 enabled on the local system.
 	 * 
 	 * @param info
+	 *          A pointer to a buffer that specifies an IP_INTERFACE_INFO
+	 *          structure that receives the list of adapters. This buffer must be
+	 *          allocated by the caller.
 	 * @param size
-	 * @return
+	 *          A pointer to a DWORD variable that specifies the size of the
+	 *          buffer pointed to by pIfTable parameter to receive the
+	 *          IP_INTERFACE_INFO structure. If this size is insufficient to hold
+	 *          the IPv4 interface information, GetInterfaceInfo fills in this
+	 *          variable with the required size, and returns an error code of
+	 *          ERROR_INSUFFICIENT_BUFFER.
+	 * @return if the function succeeds, the return value is NO_ERROR
 	 */
-	public static native int getInterfaceInfo(IpInterfaceInfo info,
-	    PcapInteger size);
+	public static native int getInterfaceInfo(MSIpInterfaceInfo info, JNumber size);
 
 }
