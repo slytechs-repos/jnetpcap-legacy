90d70a1c79179db1e44e4ce7e098e473d3738bad
voytechs <voytechs@905ad56a-9210-0410-a722-a9c141187570>
2008-12-21 18:21:40 +0000

    Added native memory usage statistics.

--- trunk/src/c/nio_jmemory.h
+++ trunk/src/c/nio_jmemory.h
@@ -7,9 +7,21 @@ extern "C" {
 #define	EXTERN extern "C"
 #endif
 	
+#include <stdint.h>
 #include "export.h"
 	
 #include <jni.h>
+	
+typedef struct memory_usage_t {
+	uint64_t total_allocated;
+	uint64_t total_deallocated;
+		
+	uint64_t total_allocate_calls;
+	uint64_t total_deallocate_calls;
+	
+	uint64_t seg_0_255_bytes;
+	uint64_t seg_256_or_above_bytes;		
+} memory_usage_t;
 
 extern	jclass jmemoryClass;
 
@@ -19,11 +31,13 @@ extern	jfieldID jmemorySizeFID;
 extern	jfieldID jmemoryOwnerFID;
 extern	jfieldID jmemoryKeeperFID;
 
+extern memory_usage_t memory_usage;
 
 // Prototypes
 void *getJMemoryPhysical(JNIEnv *env, jobject obj);
 void setJMemoryPhysical(JNIEnv *env, jobject obj, jlong value);
 void jmemoryCleanup(JNIEnv *env, jobject obj);
+
 
 #ifdef __cplusplus
 }
