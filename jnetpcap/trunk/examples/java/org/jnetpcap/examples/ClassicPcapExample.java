f60aa9fa0dced1a62510797772174142462ea4fd
voytechs <voytechs@905ad56a-9210-0410-a722-a9c141187570>
2009-11-03 14:50:39 +0000

    Updated example to account for device.getDescription not being avaiable.

--- jnetpcap/trunk/examples/java/org/jnetpcap/examples/ClassicPcapExample.java
+++ jnetpcap/trunk/examples/java/org/jnetpcap/examples/ClassicPcapExample.java
@@ -36,13 +36,13 @@ import org.jnetpcap.PcapIf;
 @SuppressWarnings("deprecation")
 public class ClassicPcapExample {
 
-  public static void main(String[] args) {
+	public static void main(String[] args) {
 		List<PcapIf> alldevs = new ArrayList<PcapIf>(); // Will be filled with NICs
-		StringBuilder errbuf = new StringBuilder();     // For any error msgs
-		
-		/********************************************
+		StringBuilder errbuf = new StringBuilder(); // For any error msgs
+
+		/***************************************************************************
 		 * First get a list of devices on this system
-		 ********************************************/
+		 **************************************************************************/
 		int r = Pcap.findAllDevs(alldevs, errbuf);
 		if (r == Pcap.NOT_OK || alldevs.isEmpty()) {
 			System.err.printf("Can't read list of devices, error is %s", errbuf
@@ -54,51 +54,61 @@ public class ClassicPcapExample {
 
 		int i = 0;
 		for (PcapIf device : alldevs) {
-			System.out.printf("#%d: %s [%s]\n", i++, device.getName(), device
-			    .getDescription());
+			String description =
+			    (device.getDescription() != null) ? device.getDescription()
+			        : "No description available";
+			System.out.printf("#%d: %s [%s]\n", i++, device.getName(), description);
 		}
 
-		PcapIf device = alldevs.get(2); // We know we have atleast 1 device
-		System.out.printf("\nChoosing '%s' on your behalf:\n", device
-		    .getDescription());
-	
-		/***************************************
+		PcapIf device = alldevs.get(0); // We know we have atleast 1 device
+		System.out
+		    .printf("\nChoosing '%s' on your behalf:\n",
+		        (device.getDescription() != null) ? device.getDescription()
+		            : device.getName());
+
+		/***************************************************************************
 		 * Second we open up the selected device
-		 ***************************************/
-		int snaplen = 64 * 1024;           // Capture all packets, no trucation
+		 **************************************************************************/
+		int snaplen = 64 * 1024; // Capture all packets, no trucation
 		int flags = Pcap.MODE_PROMISCUOUS; // capture all packets
-		int timeout = 10 * 1000;           // 10 seconds in millis
-		Pcap pcap = Pcap
-		    .openLive(device.getName(), snaplen, flags, timeout, errbuf);
+		int timeout = 10 * 1000; // 10 seconds in millis
+		Pcap pcap =
+		    Pcap.openLive(device.getName(), snaplen, flags, timeout, errbuf);
 
 		if (pcap == null) {
 			System.err.printf("Error while opening device for capture: "
 			    + errbuf.toString());
 			return;
 		}
-		
-		/**********************************************************************
+
+		/***************************************************************************
 		 * Third we create a packet hander which will be dispatched to from the
 		 * libpcap loop.
-		 **********************************************************************/
+		 **************************************************************************/
 		PcapHandler<String> printSummaryHandler = new PcapHandler<String>() {
 
-			public void nextPacket(String user, long seconds, int useconds,
-			    int caplen, int len, ByteBuffer buffer) {
-				Date timestamp = new Date(seconds * 1000 + useconds/1000); // In millis
+			public void nextPacket(
+			    String user,
+			    long seconds,
+			    int useconds,
+			    int caplen,
+			    int len,
+			    ByteBuffer buffer) {
+				Date timestamp = new Date(seconds * 1000 + useconds / 1000); // In
+				// millis
 
 				System.out.printf("Received packet at %s caplen=%-4d len=%-4d %s\n",
 				    timestamp.toString(), // timestamp to 1 ms accuracy
 				    caplen, // Length actually captured
-				    len,    // Original length of the packet
-				    user    // User supplied object
+				    len, // Original length of the packet
+				    user // User supplied object
 				    );
 			}
 		};
 
-		/************************************************************
+		/***************************************************************************
 		 * Fourth we enter the loop and tell it to capture 10 packets
-		 ************************************************************/
+		 **************************************************************************/
 		pcap.loop(10, printSummaryHandler, "jNetPcap rocks!");
 
 		/*
