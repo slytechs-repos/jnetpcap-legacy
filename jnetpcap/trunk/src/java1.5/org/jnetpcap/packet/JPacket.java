d0ad0e7f84b58b73cb7fccf102028bc580a0e8be
voytechs <voytechs@905ad56a-9210-0410-a722-a9c141187570>
2010-09-15 20:05:38 +0000

    Fixed bug#3067096 - JPacket.toString() exceptions consume all output in progress

--- jnetpcap/trunk/src/java1.5/org/jnetpcap/packet/JPacket.java
+++ jnetpcap/trunk/src/java1.5/org/jnetpcap/packet/JPacket.java
@@ -1141,11 +1141,8 @@ public abstract class JPacket
 		try {
 			out.format(this);
 			return out.toString();
-		} catch (IOException e) {
-			throw new IllegalStateException(
-			    "internal error, StringBuilder threw IOException");
-		} catch (RuntimeException e) {
-			return out.toString();
+		} catch (Exception e) {
+			throw new RuntimeException(out.toString(), e);
 		}
 	}
 }
