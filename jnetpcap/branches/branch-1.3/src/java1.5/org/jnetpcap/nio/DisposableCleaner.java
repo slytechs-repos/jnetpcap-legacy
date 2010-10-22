30b33ea3e96dae0da0bb60245015e907e753871a
voytechs <voytechs@905ad56a-9210-0410-a722-a9c141187570>
2010-10-22 15:32:07 +0000

    Added DisposableCleaner implementation based on com.sun.misc.Cleaner class which works directly with Reference collector thread. There was no significant performance gain in using the cleaner method vs. normal PhatomReference/ReferenceQueue method. The code is checked in to allow easy swapout of algorithms if needed.

---
+++ jnetpcap/branches/branch-1.3/src/java1.5/org/jnetpcap/nio/DisposableCleaner.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package org.jnetpcap.nio;
+
+import sun.misc.Cleaner;
+
+/**
+ * @author markbe
+ * 
+ */
+public abstract class DisposableCleaner implements Disposable, Runnable {
+	
+	private final Cleaner cleaner;
+	private boolean remove = false;
+
+	public DisposableCleaner(Object referent) {
+		cleaner = Cleaner.create(referent, this);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Runnable#run()
+	 */
+	@Override
+	public void run() {
+		if (!remove) {
+			dispose();		
+		}
+	}
+	
+	public void remove() {
+		remove = true;
+		cleaner.clean();
+	}
+}
