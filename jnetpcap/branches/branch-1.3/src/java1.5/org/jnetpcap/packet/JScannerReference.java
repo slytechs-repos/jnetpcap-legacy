2e68ac79fadc030fbc7f0be5dbcda167ace07b6d
voytechs <voytechs@905ad56a-9210-0410-a722-a9c141187570>
2010-10-22 15:39:28 +0000

    Renamed native dispose() method to disposeNative().

--- jnetpcap/branches/branch-1.3/src/java1.5/org/jnetpcap/packet/JScannerReference.java
+++ jnetpcap/branches/branch-1.3/src/java1.5/org/jnetpcap/packet/JScannerReference.java
@@ -24,7 +24,6 @@ public class JScannerReference extends JMemoryReference {
 	 * all the JHeaderScanners that are kept as global references need to be
 	 * released.
 	 */
-	@Override
-	public native void dispose();
+	public native void disposeNative();
 
 }
