6e2f8aa0e846d1918850e1efdbaef25420015670
voytechs <mark@slytechs.com>
2010-11-22 14:57:19 +0000

    Removed debug related delay in maxDirectMemoryBreached().

--- jnetpcap/branches/branch-1.3/src/java1.5/org/jnetpcap/nio/JMemory.java
+++ jnetpcap/branches/branch-1.3/src/java1.5/org/jnetpcap/nio/JMemory.java
@@ -110,13 +110,6 @@ public abstract class JMemory {
 	 * JNI space.
 	 */
 	private static void maxDirectMemoryBreached() {
-
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		DisposableGC.getDeault().invokeSystemGCAndWait();
 	}
 
