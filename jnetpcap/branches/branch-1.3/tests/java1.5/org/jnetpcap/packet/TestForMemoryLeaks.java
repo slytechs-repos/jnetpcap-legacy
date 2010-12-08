a1a8492172ec56b3a11e0fcd1e900b7c343abbe4
voytechs <mark@slytechs.com>
2010-12-08 22:16:08 +0000

    Updated file header copyright and LGPLv3 notice.

--- jnetpcap/branches/branch-1.3/tests/java1.5/org/jnetpcap/packet/TestForMemoryLeaks.java
+++ jnetpcap/branches/branch-1.3/tests/java1.5/org/jnetpcap/packet/TestForMemoryLeaks.java
@@ -1,14 +1,20 @@
-/**
- * Copyright (C) 2008 Sly Technologies, Inc. This library is free software; you
- * can redistribute it and/or modify it under the terms of the GNU Lesser
- * General Public License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version. This
- * library is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details. You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+/*
+ * Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010 Sly Technologies, Inc.
+ *
+ * This file is part of jNetPcap.
+ *
+ * jNetPcap is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as 
+ * published by the Free Software Foundation, either version 3 of 
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.jnetpcap.packet;
 
@@ -87,9 +93,9 @@ public class TestForMemoryLeaks extends TestUtils {
 	private static final int GENERAL_SCAN_TRANSFERTO_Q_1M =
 			60 * GENERAL_SCAN_TRANSFERTO__Q_1S; // 0:0:1
 
-	private static final int COUNT = 6 * 60 * 8 * GENERAL_SCAN_TRANSFERTO_1M;
+	private static final int COUNT = 2* GENERAL_SCAN_TRANSFERTO_1M;
 
-	private static final int LINES = 200;
+	private static final int LINES = 10;
 
 	private StringBuilder errbuf;
 
@@ -703,9 +709,9 @@ public class TestForMemoryLeaks extends TestUtils {
 				// long index = total + count;
 				// System.out.printf("#%d", index);
 
-				// b += buffer.size();
-				// PcapPacket pkt = new PcapPacket(header, buffer);
-				// pkt.scan(Ethernet.ID);
+				 b += buffer.size();
+				 PcapPacket pkt = new PcapPacket(header, buffer);
+				 pkt.scan(Ethernet.ID);
 				// h += pkt.getState().getHeaderCount();
 
 				// if (queue.remainingCapacity() == 0) {
@@ -719,7 +725,7 @@ public class TestForMemoryLeaks extends TestUtils {
 
 				// packet.peer(header, buffer);
 
-				b += packet.transferHeaderAndDataFrom(header, buffer);
+//				b += packet.transferHeaderAndDataFrom(header, buffer);
 				// new Object(){};
 				// tcpScanner.scan(buffer);
 				// h += tcpScanner.getHCount();
