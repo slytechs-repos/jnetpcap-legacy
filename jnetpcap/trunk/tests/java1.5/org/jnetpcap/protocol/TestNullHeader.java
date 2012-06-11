d11929462deee23f37c1ff37db3ed6c6d7ac5af6
voytechs <mark@slytechs.com>
2012-06-11 14:47:48 +0000

    Added a second test for packet.hasHeader(new NullHeader()) to check by object not just by ID.

--- jnetpcap/trunk/tests/java1.5/org/jnetpcap/protocol/TestNullHeader.java
+++ jnetpcap/trunk/tests/java1.5/org/jnetpcap/protocol/TestNullHeader.java
@@ -3,9 +3,11 @@
  */
 package org.jnetpcap.protocol;
 
+import junit.framework.TestCase;
+
 import org.jnetpcap.packet.JPacket;
-import org.jnetpcap.packet.JRegistry;
 import org.jnetpcap.packet.TestUtils;
+import org.jnetpcap.protocol.lan.NullHeader;
 import org.junit.Test;
 
 /**
@@ -14,15 +16,21 @@ import org.junit.Test;
  */
 public class TestNullHeader {
 	
-	private final static String FILE = "Z:\\data/DLT_NULL.rtp.cap";
+	private final static String FILE = "tests/DLT_NULL.rtp.cap";
 
 	@Test
-	public void test() {
+	public void testNullHeader() {
+		
+		NullHeader nh = new NullHeader();
 		
 //		System.out.println(JRegistry.toDebugString());
 		for (JPacket packet: TestUtils.getIterable(FILE)) {
-			System.out.println(packet);
-			break;
+//			System.out.println(packet);
+//			System.out.println(packet.getState().toDebugString());
+//			break;
+			
+			TestCase.assertTrue("NullHeader by ID missing", packet.hasHeader(JProtocol.NULL_HEADER_ID));
+			TestCase.assertTrue("NullHeader by object missing", packet.hasHeader(nh));
 		}
 	}
 
