27129e126e1fedc666f8a919f1cba970616064eb
voytechs <mark@slytechs.com>
2012-06-11 16:42:03 +0000

    Added PCAP file to tests directory for NullHeader and updated references in TestNullHeader jUnit testcase

--- jnetpcap/trunk/tests/java1.5/org/jnetpcap/protocol/TestNullHeader.java
+++ jnetpcap/trunk/tests/java1.5/org/jnetpcap/protocol/TestNullHeader.java
@@ -11,26 +11,33 @@ import org.jnetpcap.protocol.lan.NullHeader;
 import org.junit.Test;
 
 /**
- *
+ * Tests for NullHeader compliance verification
+ * 
  * @author Sly Technologies Inc.
  */
 public class TestNullHeader {
-	
+
+	/**
+	 * PCAP file generated using <em>rtpproxy</em> tool.
+	 * 
+	 * @see http://www.rtpproxy.org
+	 */
 	private final static String FILE = "tests/DLT_NULL.rtp.cap";
 
+	/**
+	 * Test proper NullHeader presence. NullHeader is a header with ID > 32,
+	 * which needs to be verified that is properly bit encoded in bit-mask.
+	 */
 	@Test
 	public void testNullHeader() {
-		
+
 		NullHeader nh = new NullHeader();
-		
-//		System.out.println(JRegistry.toDebugString());
-		for (JPacket packet: TestUtils.getIterable(FILE)) {
-//			System.out.println(packet);
-//			System.out.println(packet.getState().toDebugString());
-//			break;
-			
-			TestCase.assertTrue("NullHeader by ID missing", packet.hasHeader(JProtocol.NULL_HEADER_ID));
-			TestCase.assertTrue("NullHeader by object missing", packet.hasHeader(nh));
+
+		for (JPacket packet : TestUtils.getIterable(FILE)) {
+			TestCase.assertTrue("NullHeader by ID missing",
+					packet.hasHeader(JProtocol.NULL_HEADER_ID));
+			TestCase.assertTrue("NullHeader by object missing",
+					packet.hasHeader(nh));
 		}
 	}
 
